852501f63d11167110716ec23eb0aa7c723cce41
Zachary Hetrick <Zetrick@Zacharys-MacBook-Air.local>
2015-08-31 20:19:50 -0400

    Initial Commit

---
+++ Tower of Hanoi Solver/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Tower of Hanoi Solver
+//
+//  Created by Zachary Hetrick on 8/31/15.
+//  Copyright (c) 2015 Zetrick. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
